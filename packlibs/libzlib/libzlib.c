e9f2eddd44d7f3f6c1e51f42f07c19b65775e729
cher-nov <stackoverflow@list.ru>
2015-07-16 04:25:10 +1000

    Added libzlib plugin - a wrapper around zlib.

---
+++ packlibs/libzlib/libzlib.c
@@ -0,0 +1,72 @@
+/*
+  libzlib.c
+  (c) KoDi studio, 2015
+*/
+
+#define Z_PREFIX
+
+#include <stdlib.h>
+#include <memory.h>
+#include "zlib/zlib.h"
+#include "libzlib.h"
+
+int lib_error = Z_OK;
+unsigned int lib_compsize = 0;
+
+/* initialization functions */
+
+int get_name() {
+  return DLL_NAME;
+}
+
+int get_version() {
+  return DLL_VERSION;
+}
+
+/* compress functions */
+
+void* up_pack( void* data, int size_data ) {
+  if (lib_compsize > 0) {
+    lib_error = Z_ERRNO;
+    return NULL;
+  }
+
+  uLongf outlen = z_compressBound( size_data );
+  Bytef* outbuf = malloc( outlen );
+  lib_error = z_compress2( outbuf, &outlen, (Bytef*)data, size_data,
+                           Z_BEST_COMPRESSION );
+
+  if ( lib_error == Z_OK ) {
+    lib_compsize = outlen;
+    return realloc( outbuf, outlen );
+  } else {
+    lib_compsize = 0;
+    free( outbuf );
+    return NULL;
+  }
+}
+
+void* up_unpack( void* data, int size_data, int out_size ) {
+  Bytef* outbuf = malloc( out_size );
+  uLongf outlen = out_size;
+  lib_error = z_uncompress( outbuf, &outlen, (Bytef*)data, size_data );
+
+  if ( lib_error == Z_OK ) {
+    return (void*)outbuf;
+  } else {
+    free( outbuf );
+    return NULL;
+  }
+}
+
+/* errors functions */
+
+int get_err() {
+  int err = lib_error;
+  lib_error = Z_OK;
+  return err;
+}
+
+const char* err_str( int a_error ) {
+  return z_zError( a_error );
+}
---
+++ packlibs/libzlib/libzlib.h
@@ -0,0 +1,35 @@
+/*
+  libzlib.h
+  (c) KoDi studio, 2015
+*/
+
+#ifndef __LIBZLIB_H__
+#define __LIBZLIB_H__
+
+#include <stdio.h>
+
+#ifndef DLL_EXPORT
+  #if defined(_WIN32)
+    #define DLL_EXPORT __cdecl __declspec( dllexport )
+  #endif
+  #if defined(__APPLE__) && defined(__MACH__)
+    #define DLL_EXPORT __cdecl __attribute__(( visibility( "default" )))
+  #endif
+  #ifndef DLL_EXPORT
+    #error "OS not supported!"
+  #endif
+#endif
+
+#define DLL_NAME 'ZLIB'
+#define DLL_VERSION 128
+
+DLL_EXPORT int get_name();
+DLL_EXPORT int get_version();
+
+DLL_EXPORT void* up_pack( void*, int );
+DLL_EXPORT void* up_unpack( void*, int, int );
+
+DLL_EXPORT int get_err();
+DLL_EXPORT const char* err_str( int );
+
+#endif // __LIBZLIB_H__
