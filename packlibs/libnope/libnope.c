77e329d3a282dde648788da5cb4b165be101c94b
cher-nov <stackoverflow@list.ru>
2015-07-17 05:36:11 +1000

    Added memory reallocation function to libnope.

--- packlibs/libnope/libnope.c
+++ packlibs/libnope/libnope.c
@@ -72,7 +72,11 @@ const char* err_str( int errlev ) {
   return "unknown error";
 }
 
-/* additional */
+/* memory management */
+
+void* realloc_mem( void* ptr, size_t size_new ) {
+  return realloc( ptr, size_new );
+}
 
 void free_mem( void* ptr ) {
   free( ptr );
