c60bbf5eab2ae550a7c28f4d6df17c254f4669ad
cher-nov <stackoverflow@list.ru>
2016-01-05 17:46:16 +1000

    Now libzlib can pack and unpack simultaneously.

--- packlibs/libzlib/libzlib.c
+++ packlibs/libzlib/libzlib.c
@@ -19,7 +19,8 @@
 static int lib_error = Z_OK;
 static up_datasize_t lib_pack_size = 0;
 static up_datasize_t lib_unpack_size = 0;
-static z_stream lib_zlib_stream;
+static z_stream lib_zstream_pack;
+static z_stream lib_zstream_unpack;
 
 const char* up_info_name() {
   return DLL_NAME;
@@ -43,29 +44,29 @@ const char* up_last_error() {
 
 void up_pack_init( up_datasize_t pack_size ) {
   lib_pack_size = pack_size;
-  lib_zlib_stream.zalloc = Z_NULL;
-  lib_zlib_stream.zfree = Z_NULL;
-  lib_zlib_stream.opaque = Z_NULL;
-  z_deflateInit( &lib_zlib_stream, Z_BEST_COMPRESSION );
+  lib_zstream_pack.zalloc = Z_NULL;
+  lib_zstream_pack.zfree = Z_NULL;
+  lib_zstream_pack.opaque = Z_NULL;
+  z_deflateInit( &lib_zstream_pack, Z_BEST_COMPRESSION );
 }
 
 size_t up_pack_chunk(
   void* chunk_ptr, size_t chunk_size, void* outbuf_ptr, size_t outbuf_size
 ) {
-  lib_zlib_stream.next_in = chunk_ptr;
-  lib_zlib_stream.avail_in = chunk_size;
-  lib_zlib_stream.next_out = outbuf_ptr;
-  lib_zlib_stream.avail_out = outbuf_size;
+  lib_zstream_pack.next_in = chunk_ptr;
+  lib_zstream_pack.avail_in = chunk_size;
+  lib_zstream_pack.next_out = outbuf_ptr;
+  lib_zstream_pack.avail_out = outbuf_size;
 
   int result, flush;
   flush = (chunk_size < lib_pack_size) ? Z_NO_FLUSH : Z_FINISH;
-  result = z_deflate( &lib_zlib_stream, flush );
+  result = z_deflate( &lib_zstream_pack, flush );
   size_t packed_size;
 
   switch (result) {
     case Z_OK:
     case Z_STREAM_END:
-      packed_size = chunk_size - lib_zlib_stream.avail_in;
+      packed_size = chunk_size - lib_zstream_pack.avail_in;
       lib_pack_size -= packed_size;
       return packed_size;
     break;
@@ -77,7 +78,7 @@ size_t up_pack_chunk(
 }
 
 void up_pack_end() {
-  z_deflateEnd( &lib_zlib_stream );
+  z_deflateEnd( &lib_zstream_pack );
   lib_pack_size = 0;
 }
 
@@ -85,30 +86,30 @@ void up_pack_end() {
 
 void up_unpack_init( up_datasize_t unpack_size ) {
   lib_unpack_size = unpack_size;
-  lib_zlib_stream.zalloc = Z_NULL;
-  lib_zlib_stream.zfree = Z_NULL;
-  lib_zlib_stream.opaque = Z_NULL;
-  lib_zlib_stream.avail_in = 0;
-  lib_zlib_stream.next_in = Z_NULL;
-  z_inflateInit( &lib_zlib_stream );
+  lib_zstream_unpack.zalloc = Z_NULL;
+  lib_zstream_unpack.zfree = Z_NULL;
+  lib_zstream_unpack.opaque = Z_NULL;
+  lib_zstream_unpack.avail_in = 0;
+  lib_zstream_unpack.next_in = Z_NULL;
+  z_inflateInit( &lib_zstream_unpack );
 }
 
 size_t up_unpack_chunk(
   void* chunk_ptr, size_t chunk_size, void* outbuf_ptr, size_t outbuf_size
 ) {
-  lib_zlib_stream.next_in = chunk_ptr;
-  lib_zlib_stream.avail_in = chunk_size;
-  lib_zlib_stream.next_out = outbuf_ptr;
-  lib_zlib_stream.avail_out = outbuf_size;
+  lib_zstream_unpack.next_in = chunk_ptr;
+  lib_zstream_unpack.avail_in = chunk_size;
+  lib_zstream_unpack.next_out = outbuf_ptr;
+  lib_zstream_unpack.avail_out = outbuf_size;
 
   int result;
-  result = z_inflate( &lib_zlib_stream, Z_NO_FLUSH );
+  result = z_inflate( &lib_zstream_unpack, Z_NO_FLUSH );
   size_t unpacked_size;
 
   switch (result) {
     case Z_OK:
     case Z_STREAM_END:
-      unpacked_size = outbuf_size - lib_zlib_stream.avail_out;
+      unpacked_size = outbuf_size - lib_zstream_unpack.avail_out;
       lib_unpack_size -= unpacked_size;
       return unpacked_size;
     break;
@@ -120,7 +121,7 @@ size_t up_unpack_chunk(
 }
 
 void up_unpack_end() {
-  z_inflateEnd( &lib_zlib_stream );
+  z_inflateEnd( &lib_zstream_unpack );
   lib_unpack_size = 0;
 }
 
