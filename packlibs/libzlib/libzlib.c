ef622c3c2759b9e1ad6d7e56d41a470eb7640a49
cher-nov <stackoverflow@list.ru>
2016-04-12 03:32:27 +1000

    Added *_done() functions to the libzlib.

--- packlibs/libzlib/libzlib.c
+++ packlibs/libzlib/libzlib.c
@@ -17,8 +17,12 @@
 #endif
 
 static int lib_error = Z_OK;
-static up_datasize_t lib_pack_left = 0;
+
+static up_datasize_t lib_pack_left;
 static z_stream lib_zstream_pack;
+static bool lib_pack_flushed;
+
+static up_datasize_t lib_unpack_left;
 static z_stream lib_zstream_unpack;
 
 const char* up_info_name() {
@@ -49,11 +53,12 @@ const char* up_error_msg( int err_code ) {
 /* compression functions */
 
 void up_pack_init( up_datasize_t pack_size ) {
-  lib_pack_left = pack_size;
   lib_zstream_pack.zalloc = Z_NULL;
   lib_zstream_pack.zfree = Z_NULL;
   lib_zstream_pack.opaque = Z_NULL;
   z_deflateInit( &lib_zstream_pack, Z_BEST_COMPRESSION );
+  lib_pack_left = pack_size;
+  lib_pack_flushed = false;
 }
 
 void up_pack_chunk( void* chunk_ptr, size_t chunk_size ) {
@@ -67,13 +72,17 @@ size_t up_pack_step( void* outbuf_ptr, size_t outbuf_size, size_t* data_left ) {
 
   int result, flush;
   size_t input_size = lib_zstream_pack.avail_in;
-  flush = (lib_pack_left > input_size) ? Z_NO_FLUSH : Z_FINISH;
+  flush = (lib_pack_left > 0) ? Z_NO_FLUSH : Z_FINISH;
   result = z_deflate( &lib_zstream_pack, flush );
 
   switch (result) {
     case Z_OK:
     case Z_STREAM_END:
-      lib_pack_left -= input_size - lib_zstream_pack.avail_in;
+      if (flush == Z_FINISH) {
+        lib_pack_flushed = true;
+      } else {
+        lib_pack_left -= input_size - lib_zstream_pack.avail_in;
+      }
       if (data_left != NULL) { *data_left = lib_zstream_pack.avail_in; }
       return outbuf_size - lib_zstream_pack.avail_out;
     break;
@@ -84,9 +93,12 @@ size_t up_pack_step( void* outbuf_ptr, size_t outbuf_size, size_t* data_left ) {
   }
 }
 
+bool up_pack_done() {
+  return ( (lib_pack_left == 0) && lib_pack_flushed );
+}
+
 void up_pack_end() {
   z_deflateEnd( &lib_zstream_pack );
-  lib_pack_left = 0;
 }
 
 /* decompression functions */
@@ -98,6 +110,7 @@ void up_unpack_init( up_datasize_t unpack_size ) {
   lib_zstream_unpack.avail_in = 0;
   lib_zstream_unpack.next_in = Z_NULL;
   z_inflateInit( &lib_zstream_unpack );
+  lib_unpack_left = unpack_size;
 }
 
 void up_unpack_chunk( void* chunk_ptr, size_t chunk_size ) {
@@ -110,11 +123,13 @@ size_t up_unpack_step( void* outbuf_ptr, size_t outbuf_size, size_t* data_left )
   lib_zstream_unpack.avail_out = outbuf_size;
 
   int result;
+  size_t input_size = lib_zstream_unpack.avail_in;
   result = z_inflate( &lib_zstream_unpack, Z_NO_FLUSH );
 
   switch (result) {
     case Z_OK:
     case Z_STREAM_END:
+      lib_unpack_left -= input_size - lib_zstream_unpack.avail_in;
       if (data_left != NULL) { *data_left = lib_zstream_unpack.avail_in; }
       return outbuf_size - lib_zstream_unpack.avail_out;
     break;
@@ -123,6 +138,10 @@ size_t up_unpack_step( void* outbuf_ptr, size_t outbuf_size, size_t* data_left )
       lib_error = result;
       return 0;
   }
+}
+
+bool up_unpack_done() {
+  return (lib_unpack_left == 0);
 }
 
 void up_unpack_end() {
